c568cc68a69c83e2cd9689f0d7b1df4bef544cf1
dangmakara <dang.dangmakara@gmail.com>
2024-10-31 14:59:27 +0700

    update

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -84,7 +84,7 @@ const NavBar = ({show}:{show: any}) => {
           Orders
         </Link>
 
-        <Link href={"/settings"} className={pathname.includes("/settings") ? activeLink : InactiveLink}>
+        {/* <Link href={"/settings"} className={pathname.includes("/settings") ? activeLink : InactiveLink}>
           <svg 
             xmlns="http://www.w3.org/2000/svg" 
             fill="none" 
@@ -98,10 +98,17 @@ const NavBar = ({show}:{show: any}) => {
           </svg>
 
           Settings
-        </Link>
+        </Link> */}
 
         <button onClick={() =>Logout()} className={InactiveLink}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            strokeWidth={1.5} 
+            stroke="currentColor" 
+            className="size-6"
+          >
             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
           </svg>
 
